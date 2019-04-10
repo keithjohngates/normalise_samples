ca573d16c0744ab5e9f0f164215aff1e740a4dd1
Gates admin-Keith <Admin-gatesk@dec.int>
2019-04-10 17:01:07 +1000

    making coloured subsets

--- ipy_notebooks/normalise_worker.py
+++ ipy_notebooks/normalise_worker.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from pysal.viz.mapclassify import Natural_Breaks as nb
-from math import log10
+# from math import log10
 import pickle
 import os
 import pandas as pd
 import geopandas as gpd
+import numpy as np
 
 def calc_normalisation(group, f, elements):
 	data_out = os.path.join(os.path.dirname(os.getcwd()), 'data_out')
@@ -12,6 +13,9 @@ def calc_normalisation(group, f, elements):
 	normalised = gpd.GeoDataFrame()
     
 	for subset, samples in group.groupby(['RIN', 'index']):
+	
+		print(subset)
+		
 		df = normalise(samples, elements)
 		normalised = gpd.GeoDataFrame(pd.concat([normalised, df], ignore_index=True))
 
@@ -21,57 +25,55 @@ def calc_normalisation(group, f, elements):
 def normalise(df, elements):
 
 	for element in elements:
-		print(element)
 	
-		df[element].fillna(0, inplace=True)
+		df[element].fillna(np.nan, inplace=True)
 		
 		df['%s_nn' % element] = pd.to_numeric(df['%s' % element])
+		
+		df['%s_nn' % element].fillna(np.nan, inplace=True)
 
 		df['%s_fn' % element] = df['%s_nn' % element].apply(lambda x: neg_conversions(x))
+		
+		df['%s_fn' % element].fillna(np.nan, inplace=True)
 
-		df['%s_log' % element] = df['%s_fn' % element].apply(lambda x: log10(x))
+		df['%s_log' % element] = df['%s_fn' % element].apply(lambda x: np.log10(x))
 
 		median = df['%s_log' % element].median()
-		
-		print('median %s' % median)
 		
 		# Mean Absolute Deviation: Tukey, J.W., 1977. Exploratory Data Analysis. Addison-Wesley, Reading, 688 pp
 		mad = df['%s_log' % element].mad()
 		
-		print('mad %s' % mad)
-		
 		# Set threshold: http://crcleme.org.au/Pubs/guides/gawler/a7_id_anomalies.pdf
-		threshold = median + 2*mad
-		
-		print('threshold %s' % threshold)
+		threshold = median + (mad+mad)
 		
 		min_value = df['%s_log' % element].min()
+		max_value = df['%s_log' % element].max()
 		
-		print('min_value %s' % min_value)
+		df['%s_nml' % element] = df['%s_log' % element].apply(lambda x: scaling(x, min_value, max_value))
 		
-		df['%s_nml' % element] = df['%s_log' % element].apply(lambda x: scaling(x, min_value, threshold))
+		# histogram = df['%s_nml' % element].hist()
 		
-		print('normalised')
+
 		# classifier = nb(df['%s_log' % element], 7)
 		# df['classifications'] = df['%s_log' % element].apply(classifier)
 		# df.classifications.replace([1, 2, 3, 4, 5, 6, 7], ['#82817d', '#55b1d9', '#5bd955', '#e6a94e', '#e02d2d', '#da2de0', '#af00b5'], inplace=True)
-		
+	print('Normalised')
 	return df
 
 def neg_conversions(x):
 	# Define the negative conversions
-	if x < -5:
-		return 0.0005
+	if x < -1:
+		return np.nan
 	if x < 0:
 		return abs(x)/2
 	if x == 0:
-		return 0.0005
+		return np.nan
 	if x > 0:
 		return x
 
 
-def scaling(x, min_value, threshold):
+def scaling(x, min_value, max_value): # tried the threshold didn't work
 	# Normalisation of values between 0 and 1
 	
-	x = (x - min_value) / (threshold - min_value)  # 'Max' value is the threshold
+	x = (x - min_value) / (max_value - min_value)  # 'Max' value is the threshold
 	return x
